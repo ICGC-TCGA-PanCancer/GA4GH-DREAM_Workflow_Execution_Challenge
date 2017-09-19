4fb667aead001a1311fea87593ce6739a71b9e3a
Brice Aminou <brice.aminou@gmail.com>
2017-09-19 10:10:15 -0400

    Other files added to the checker

--- pcawg-bwa-mem-aligner/checker/pcawg-bwa-mem-result-checker.sh
+++ pcawg-bwa-mem-aligner/checker/pcawg-bwa-mem-result-checker.sh
@@ -2,6 +2,18 @@
 
 normal_bam1=$1
 normal_bam2=$2
+unmapped_bam1=$3
+unmapped_bam2=$4
+normal_bai1=$5
+normal_bai2=$6
+unmapped_bai1=$7
+unmapped_bai2=$8
+normal_metrics1=$9
+normal_metrics2=${10}
+unmapped_metrics1=${11}
+unmapped_metrics2=${12}
+
+
 
 function check_md5 {
 	md5_1=`md5sum $1 | cut -d' ' -f1`
@@ -17,6 +29,11 @@ function check_md5 {
 flag=0
 
 if [ $(check_md5 <(/usr/bin/samtools view -f 64 $normal_bam1) <(/usr/bin/samtools view -f 64 $normal_bam2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$normal_bam2 >> checker_report.log;flag=1;fi
+if [ $(check_md5 <(/usr/bin/samtools view -f 64 $unmapped_bam1) <(/usr/bin/samtools view -f 64 $unmapped_bam2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$unmapped_bam2 >> checker_report.log;flag=1;fi
+if [ $(check_md5 <(cat $normal_bai1) <(cat $normal_bai2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$normal_bai2 >> checker_report.log;flag=1;fi
+if [ $(check_md5 <(cat $unmapped_bai1) <(cat $unmapped_bai2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$unmapped_bai2 >> checker_report.log;flag=1;fi
+if [ $(check_md5 <(cat $normal_metrics1) <(cat $normal_metrics2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$normal_metrics2 >> checker_report.log;flag=1;fi
+if [ $(check_md5 <(cat $unmapped_metrics1) <(cat $unmapped_metrics2)) -eq 0 ];then echo "overall: false" > checker_report.log;echo "mismatch result found in "$unmapped_metrics2 >> checker_report.log;flag=1;fi
 
 if [ $flag -eq 0 ]
 then
